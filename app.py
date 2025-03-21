38aedd6ab7022ab3abc9a272a3e9ea3e97649258
mewset <86.mattias@gmail.com>
2025-03-21 15:28:12 +0100

    removed testroute

--- app.py
+++ app.py
@@ -152,10 +152,6 @@ def start_scheduler():
 def index():
     return render_template("index.html", current_date=datetime.now().strftime("%Y-%m-%d"))
 
-@app.route("/testindex")
-def testindex():
-    return render_template("testindex.html", current_date=datetime.now().strftime("%Y-%m-%d"))
-
 @app.route("/admin")
 def admin():
     return render_template("admin.html", current_date=datetime.now().strftime("%Y-%m-%d"))
